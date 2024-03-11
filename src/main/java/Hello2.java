0cf7546ee24a7368c014b78a84813704bbf4b48f
junsik0408 <lccdc@naver.com>
2024-03-11 15:26:10 +0900

    두번째 파일

--- src/main/java/Hello2.java
+++ src/main/java/Hello2.java
@@ -1,6 +1,5 @@
-public class Hello2
-
-Public Static void main(string[] args){
-
-    System.out Println("두번째 생성한 파일");
+public class Hello2{
+    public static void main(String[] args) {
+        System.out.println("두번째 생성한 파일");
+    }
 }
