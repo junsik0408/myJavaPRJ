374ba1be5141698b58dfd40827df5403747e48ce
junsik0408 <lccdc@naver.com>
2024-03-19 08:56:51 +0900

    Code319

---
+++ src/main/java/Code_3195.java
@@ -0,0 +1,9 @@
+public class Code_3195 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c );
+    }
+}
