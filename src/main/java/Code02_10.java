b256062ef784eb90d4113b5d4de7c72d8eb307ed
junsik0408 <lccdc@naver.com>
2024-03-18 15:19:32 +0900

    Code02_07

--- src/main/java/Code02_10.java
+++ src/main/java/Code02_10.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Code02_10 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -7,10 +6,8 @@ public class Code02_10 {
         int num;
 
         num = s.nextInt();
-
         System.out.println(("사용자가 입력한 값==>" + num));
 
         s.close();
-
     }
 }
