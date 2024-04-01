27e03da8933b37d81d7819bb0ba7fc366029ade8
junsik0408 <lccdc@naver.com>
2024-04-01 14:21:14 +0900

    4_9

---
+++ src/main/java/Code5_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Code5_5 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int num;
+
+        System.out.print("숫자 입력 ==>");
+        num = S.nextInt();
+
+        if (num%2 == 0)
+            System.out.println("짝수입니다.");
+        else
+            System.out.println("홀수입니다.");
+
+        S.close();
+
+    }
+}
