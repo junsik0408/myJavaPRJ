f4fd0b0ee632ccf3235a2a4537d0bbe93b81e062
junsik0408 <lccdc@naver.com>
2024-04-01 13:38:36 +0900

    4_9

---
+++ src/main/java/Code05_2.java
@@ -0,0 +1,10 @@
+public class Code05_2 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.print("100보다");
+            System.out.print("작습니다.");
+        }
+    }
+}
