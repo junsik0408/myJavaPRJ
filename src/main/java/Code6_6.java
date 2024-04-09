799a58d4e5cc457ebc735465874cc0dced320efb
junsik0408 <lccdc@naver.com>
2024-04-09 15:50:11 +0900

    6-3

---
+++ src/main/java/Code6_6.java
@@ -0,0 +1,13 @@
+public class Code6_6 {
+    public static void main(String[] args) {
+        int hap = 0
+
+        for (int i = 1; i <= 10; i++) {
+            hap += i;
+
+            System.out.println("hap : " + hap);
+        }
+        System.out.println("1부터 10까지 합계는 " + hap);
+    }
+
+}
