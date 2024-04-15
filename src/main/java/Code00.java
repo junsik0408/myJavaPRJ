87379f1085a4139fe3300e4d2a3161c19aa63437
junsik0408 <lccdc@naver.com>
2024-04-15 14:23:00 +0900

    00

---
+++ src/main/java/Code00.java
@@ -0,0 +1,11 @@
+public class Code00 {
+    public static void main(String[] args) {
+
+        for (int k = 2; k <= 9; k++) {
+
+            for (int i = 1; i<= 9; i++) {
+                System.out.println(k + " X " + i + " = " + k * i);
+            }
+        }
+    }
+}
