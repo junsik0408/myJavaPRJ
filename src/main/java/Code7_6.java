e41476cc21e42000c3d27192c77b390c15d4bf51
junsik0408 <lccdc@naver.com>
2024-05-07 14:11:59 +0900

    0000

---
+++ src/main/java/Code7_6.java
@@ -0,0 +1,16 @@
+class Rabbit {
+    String shape; // 토끼모양
+    int xPos;
+    int yPos;
+
+    void  setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code7_6 {
+    public static void main(String[] args) {
+
+    }
+}
