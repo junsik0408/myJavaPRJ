5418139a217bc5f58f1e14592e958092bc430f2f
junsik0408 <lccdc@naver.com>
2024-05-07 14:27:40 +0900

    0000

--- src/main/java/Code7_6.java
+++ src/main/java/Code7_6.java
@@ -6,11 +6,20 @@ class Rabbit {
     void  setPosition(int x, int y) {
         xPos = x;
         yPos = y;
+
+        System.out.println("xPos: " + xPos + " / yPos : " + this.yPos);
     }
 }
 
 public class Code7_6 {
     public static void main(String[] args) {
+
+        Rabbit rb1 = new Rabbit();
+        rb1.setPosition(100, 100);
+
+        Rabbit rb2 = new Rabbit();
+        rb1.setPosition(200, 200);
+
 
     }
 }
