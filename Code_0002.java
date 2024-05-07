b3e03a0b4ff1fbd8024681fc855a3bbc4a406fa7
junsik0408 <162661899+junsik0408@users.noreply.github.com>
2024-05-07 12:46:04 +0900

    Add files via upload

---
+++ Code_0002.java
@@ -0,0 +1,14 @@
+public class Code_0002 {
+    public static void main(String[] args) {
+        int grade=93;
+
+                if(grade>=90) //조건식
+                    System.out.print("A입니다.");  //조건식이 참이면 실행, 거짓이면 다음 문장으로 이동
+                else if(grade>=80)
+                    System.out.print("B입니다.");  //조건식이 참이면 실행, 거짓이면 다음 문장으로 이동
+                else
+                    System.out.print("C입니다.");  //모든 조건에 거짓인경우 실행
+            }
+        }
+
+
