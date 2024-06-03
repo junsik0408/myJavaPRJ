7dc7b271f3c4766fd67b906dd93b7b6138bfee56
junsik0408 <lccdc@naver.com>
2024-06-03 14:42:19 +0900

    0000

--- src/main/java/Code_0005.java
+++ src/main/java/Code_0005.java
@@ -1,7 +1,7 @@
 public class Code_0005 {
     public static void main(String[] args) {
         for (int i = 2, j = 1; j < 10; i++) { // for문 변수 2개로 지정 조건식 증감식을 다른 변수로 지정
-            if (i == 10) { // i가 증감식으로 10
+            if (i == 10) { // i가 증감 10
                 i = 1; // 1로 초기화 , 증감식 2부터 시작
                 System.out.println(); // i가 9까지 출력되고 증감식으로 10이 되므로 줄바꿈
                 j++; // j를 증감 , for문 반복문 성립
