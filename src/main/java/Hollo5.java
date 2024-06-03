4b319c85ecb673eed6d22e939ceda9aca8025d67
junsik0408 <lccdc@naver.com>
2024-06-03 14:42:31 +0900

    0000

--- src/main/java/Hollo5.java
+++
@@ -1,5 +0,0 @@
-public class Hollo5 {
-    public static void main(String[] args) {
-        System.out.println("다섯번파일");
-    }
-}
---
+++ src/main/java/org/example/Hello3.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Hello3 {
+    public static void main(String[] args) {
+    System.out.println("3번째 생성한파일");
+}
+}
+
+
