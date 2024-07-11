664afaadcab073060489009722c17c0b5ceee1d1
junsik0408 <162661899+junsik0408@users.noreply.github.com>
2024-07-11 23:13:33 +0900

    Add files via upload

--- Code_0001.java
+++ Code_0001.java
@@ -1,10 +1,17 @@
 public class Code_0001 {
-            public static void main(String args[]){
+    public static void main(String[] args) {
 
-                for(int i=0; i<4;i++){
-                    for(int j=0; j<10; j++)
-                    {System.out.print("*");}
-                    System.out.println();
-                }
-            }
+        int ary[] = {10, 20, 30};
+
+        try {
+            ary[3] = 1234/ 0;
+            ary[3] = 1234;
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("배열 첨자 오류입니다.");
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나누는 등의 수식 오류입니다.");
+        } finally {
+            System.out.println("이 부분은 무조건 보여요.");
         }
+    }
+}
