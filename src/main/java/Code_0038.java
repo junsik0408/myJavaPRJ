eb692788a4fb405ed135c892711d067f313e89be
junsik0408 <2420110169@office.kopo.ac.kr>
2024-09-11 08:59:13 +0900

    0038

---
+++ src/main/java/Code_0038.java
@@ -0,0 +1,28 @@
+public class Code_0038 {
+    public static void main(String[] args) {
+        int n = 5;  // 다이아몬드의 높이
+
+        // 윗부분 (피라미드)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아랫부분 (역피라미드)
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
