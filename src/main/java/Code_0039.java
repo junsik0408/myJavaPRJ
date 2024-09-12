14b3b15f28eb54f57b5298992ec1de00f7129f78
junsik0408 <2420110169@office.kopo.ac.kr>
2024-09-12 09:09:05 +0900

    0039

---
+++ src/main/java/Code_0039.java
@@ -0,0 +1,35 @@
+public class Code_0039 {
+        public static void main(String[] args) {
+            int n = 5;  // 나비의 크기
+
+            // 윗부분
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= 2 * (n - i); j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            // 아랫부분
+            for (int i = n; i >= 1; i--) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= 2 * (n - i); j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
