d4ead7bc01fc8def9bbf80000759230412bda95e
junsik0408 <2420110169@office.kopo.ac.kr>
2024-09-16 15:57:16 +0900

    0040

---
+++ Code_0040.java
@@ -0,0 +1,27 @@
+public class Code_0040 {
+        public static void main(String[] args) {
+            int n = 5;  // 모래시계의 높이
+
+            // 윗부분 (역피라미드)
+            for (int i = n; i >= 1; i--) {
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            // 아랫부분 (피라미드)
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
