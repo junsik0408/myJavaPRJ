aefc610c70f37f752081e142fc5b897d377ff2f4
junsik0408 <2420110169@office.kopo.ac.kr>
2024-09-20 08:41:12 +0900

    0042

---
+++ src/main/java/Code_0042.java
@@ -0,0 +1,21 @@
+
+public class Code_0042 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {5, 5, 5, 5, 5},
+                {10, 10, 10, 10, 10},
+                {20, 20, 20, 20, 20},
+                {30, 30, 30, 30, 30}
+        };
+        int total = 0;
+        float average = 0;
+
+        /* total = 325
+        average=16.25
+
+        */
+
+        System.out.println("total=" + total);
+        System.out.println("average= " + average);
+    }
+}
