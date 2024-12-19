40a0832f6f6cd933af829e7ec001c81a854e088b
junsik0408 <2420110169@office.kopo.ac.kr>
2024-12-19 17:25:59 +0900

    241219

---
+++ src/main/java/baekjun/B15963.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class B15963 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        long a = s.nextLong();
+
+        long b = s.nextLong();
+
+        if (a==b){
+            System.out.println("1");
+        }
+
+        else {
+            System.out.println("0");
+        }
+
+    }
+}
